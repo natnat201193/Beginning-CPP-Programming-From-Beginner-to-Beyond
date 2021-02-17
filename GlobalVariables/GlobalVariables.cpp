9fa5a3002fda9c05071387011882284ec142b114
natnat <33712044+natnat201193@users.noreply.github.com>
2021-02-17 15:23:51 +0000

    Section 6: Global Variables.

---
+++ GlobalVariables/GlobalVariables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//int age{ 18 }; // Global variable.
+
+int main()
+{
+    int age{18}; // Local variable.
+
+    cout << age << endl;
+
+    return 0;
+}
