b381b0064e4e15c1c412e10d6a126f9877fa07e2
natnat <33712044+natnat201193@users.noreply.github.com>
2021-02-17 20:18:24 +0000

    Section 6: Constants

---
+++ Section6_Variables_And_Constants/Constants/Constants.cpp
@@ -0,0 +1,54 @@
+// Section 6.
+// Constants.
+#include <iostream>
+using namespace std;
+
+/*
+    Frank's Carpet Cleaning Service.
+    Charges $30 per room.
+    Sales tax rate is 6%.
+    Estimates are valid for 30 days.
+
+    Promp the user for the number of rooms they would like cleaned and provide an estimate such as:
+
+    Estimate for carpet cleaning service:
+    Number of rooms: 2
+    Price per room: $30
+    Cost: $60
+    Tax: $3.6
+
+    ======================================================================
+    Total estimate: $63.6
+    This estimate is valid for 30 days.
+
+
+    Pseudocode:
+    Prompt the user to enter the number of rooms.
+    Display the number of rooms.
+    Display the price per room.
+
+    Display cost: (number of rooms * price per room)
+    Display tax: number of rooms * price per room * tax rate
+    Display total estimate: (number of rooms * price per room) + (number of rooms * price per room * tax rate)
+    Display estimate expiration time.
+*/
+
+int main()
+{
+    cout << "Hello, welcome to Frank's Carpet Cleaning Service.";
+    cout << "\n=========================================================\n";
+    cout << "How many rooms would you like cleaned? ";
+    int NumberOfRooms{ 0 };
+    cin >> NumberOfRooms;
+    cout << "\nEstimate for carpet cleaning service: \n";
+    cout << "Number of rooms: " << NumberOfRooms << endl;
+    cout << "Price per room: $" << 30 << endl;
+    cout << "Cost: $" << 30 * NumberOfRooms << endl;
+    cout << "Sales tax: $" << 30 * NumberOfRooms * 0.06 << endl;
+    cout << "\n=========================================================\n";
+    cout << "Total estimate: $" << (30 * NumberOfRooms) + (30 * NumberOfRooms * 0.06) << endl;
+    cout << "This estimate is valid for " << 30 << " days.\n";
+
+    cout << endl;
+    return 0;
+}
