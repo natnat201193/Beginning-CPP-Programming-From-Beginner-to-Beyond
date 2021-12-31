e0b5f85f5c6288a0bcb6b00141916be28d022c1a
natnat <33712044+natnat201193@users.noreply.github.com>
2021-12-31 19:33:10 +0000

    Section 4: Challenge

---
+++ Section4_2_Challenge/main.cpp
@@ -0,0 +1,15 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include <iostream>
+
+int main()
+{
+	int FavouriteNumber;
+	std::cout << "Enter your favourite number between 1 and 100: ";
+	std::cin >> FavouriteNumber;
+	std::cout << "Amazing!! That's my favourite number too!" << std::endl;
+	std::cout << "No really!!, " << FavouriteNumber << " is my favourite number!" << std::endl;
+	return 0;
+}
