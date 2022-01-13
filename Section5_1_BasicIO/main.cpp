1a33cd8c70b6e0613c1d83ea36536b32f6f3463e
natnat <33712044+natnat201193@users.noreply.github.com>
2022-01-13 12:48:41 +0000

    Section5: Basic IO using cin and cout

---
+++ Section5_1_BasicIO/main.cpp
@@ -0,0 +1,61 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include <iostream>
+
+int main()
+{
+	//std::cout << "Hello world!" << std::endl;
+
+
+	//std::cout << "Hello";
+	//std::cout << "world!" << std::endl;
+
+
+	//std::cout << "Hello world!" << std::endl;
+	//std::cout << "Hello" << " world!" << std::endl;
+	//std::cout << "Hello" << " world!\n";
+	//std::cout << "Hello\nOut\nThere\n";
+
+
+	int num1{};
+	int num2{};
+	double num3{};
+
+	//std::cout << "Enter an interger: ";
+	//std::cin >> num1;
+	//std::cout << "You entered: " << num1 << std::endl;
+
+
+	//std::cout << "Enter a first integer: ";
+	//std::cin >> num1;
+
+	//std::cout << "Enter a second integer: ";
+	//std::cin >> num2;
+
+	//std::cout << "You entered " << num1 << " and " << num2 << std::endl;
+
+
+	//std::cout << "Enter 2 integers separated with a space: ";
+	//std::cin >> num1 >> num2;
+	//std::cout << "You entered " << num1 << " and " << num2 << std::endl;
+
+
+	//std::cout << "Enter a double: ";
+	//std::cin >> num3;
+
+	//std::cout << "You entered: " << num3 << std::endl;
+
+
+	std::cout << "Enter an integer: ";
+	std::cin >> num1;
+
+	std::cout << "Enter a double: ";
+	std::cin >> num3;
+
+	std::cout << "The integer is: " << num1 << std::endl;
+	std::cout << "And the double is: " << num3 << std::endl;
+
+	return 0;
+}
