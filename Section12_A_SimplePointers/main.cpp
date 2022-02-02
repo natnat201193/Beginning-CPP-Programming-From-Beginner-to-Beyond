9a5b66086f72e41073209e795a48141bf8e70f9c
natnat <33712044+natnat201193@users.noreply.github.com>
2022-02-02 02:06:37 +0000

    Added another example to SimplePointers + some cleanup

--- Section12_A_SimplePointers/main.cpp
+++ Section12_A_SimplePointers/main.cpp
@@ -9,18 +9,28 @@
 
 int main()
 {
+    std::string Food{ "Pizza" };  // A Food variable of type string
+    std::string* Ptr { &Food }; // A pointer variable, with the name Ptr, that stores the address of Food
+    std::cout
+        << "Variable: std::string Food{ \"Pizza\" }; // A Food variable of type string" << std::endl
+        << "Pointer Variable: std::string* Ptr { &Food }; // A pointer variable, with the name Ptr, that stores the address of Food" << std::endl
+        << "Value of Food is: " << Food << std::endl // Output the value of food (Pizza)
+        << "Address of &Food is: " << &Food << std::endl // Output the memory address of food (0x6dfed4)
+        << "Address of Ptr is: " << Ptr << std::endl << std::endl; // Output the memory address of food with the pointer (0x6dfed4)
+
     int Num{ 10 };
-    std::cout << "Value of Num is: " << Num << std::endl;
-    std::cout << "sizeof of Num is: " << sizeof Num << std::endl;
-    std::cout << "Address of Num is: " << &Num << std::endl;
+    std::cout
+        << "Value of Num is: " << Num << std::endl
+        << "sizeof of Num is: " << sizeof Num << std::endl
+        << "Address of Num is: " << &Num << std::endl << std::endl;
 
     int* P{};
-    std::cout << std::endl << "Value of P is: " << P << std::endl; // this would show garbage data if P was not initialised
-    std::cout << "sizeof of P is: " << sizeof P << std::endl;
-    std::cout << "Address of P is: " << &P << std::endl;
-
+    std::cout 
+        << "Value of P is: " << P << std::endl // this would show garbage data if P was not initialised
+        << "sizeof of P is: " << sizeof P << std::endl
+        << "Address of P is: " << &P << std::endl;
     P = nullptr;
-    std::cout << std::endl << "Value of P is: " << P << std::endl;
+    std::cout << "Value of P is: " << P << std::endl << std::endl;
 
     int* P1{ nullptr };
     double* P2{ nullptr };
@@ -28,21 +38,21 @@ int main()
     std::vector<std::string>* P4{ nullptr };
     std::string* P5{ nullptr };
 
-    std::cout << std::endl << "sizeof P1 is: " << sizeof P1 << std::endl;
-    std::cout << "sizeof P2 is: " << sizeof P2 << std::endl;
-    std::cout << "sizeof P3 is: " << sizeof P3 << std::endl;
-    std::cout << "sizeof P4 is: " << sizeof P4 << std::endl;
-    std::cout << "sizeof P5 is: " << sizeof P5 << std::endl;
+    std::cout
+        << "sizeof P1 is: " << sizeof P1 << std::endl
+        << "sizeof P2 is: " << sizeof P2 << std::endl
+        << "sizeof P3 is: " << sizeof P3 << std::endl
+        << "sizeof P4 is: " << sizeof P4 << std::endl
+        << "sizeof P5 is: " << sizeof P5 << std::endl << std::endl;
 
     int Score{ 10 };
     //double HighTemp{ 100.7 };
-
     int* ScorePtr{ nullptr };
-
     ScorePtr = &Score;
-    std::cout << "Value of Score is: " << Score << std::endl;
-    std::cout << "Address of Score is: " << &Score << std::endl;
-    std::cout << "Value of ScorePtr is: " << ScorePtr << std::endl;
+    std::cout
+        << "Value of Score is: " << Score << std::endl
+        << "Address of Score is: " << &Score << std::endl
+        << "Value of ScorePtr is: " << ScorePtr << std::endl;
     // ScorePtr = &HighTemp;     // Compiler error
 
     std::cout << std::endl;
