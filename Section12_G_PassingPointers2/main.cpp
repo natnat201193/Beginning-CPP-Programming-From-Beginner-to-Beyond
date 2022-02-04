3abc57d89fdda82a56251ddf6f2e184517112aea
natnat <33712044+natnat201193@users.noreply.github.com>
2022-02-04 01:23:25 +0000

    Section 12: Passing Pointers 2

---
+++ Section12_G_PassingPointers2/main.cpp
@@ -0,0 +1,30 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+// Passing Pointers 2
+#include <iostream>
+#include <string>
+#include <vector>
+
+void swap(int* A, int* B)
+{
+    int Temp = *A;
+    *A = *B;
+    *B = Temp;
+}
+
+int main()
+{
+    int X{ 100 }, Y{ 200 };
+    std::cout << std::endl << "X: " << X << std::endl;
+    std::cout << "Y: " << Y << std::endl;
+
+    swap(&X, &Y);
+
+    std::cout << std::endl << "X: " << X << std::endl;
+    std::cout << "Y: " << Y << std::endl;
+
+    std::cout << std::endl;
+    return 0;
+}
